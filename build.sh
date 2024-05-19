4f5b9ad65689c6fad812400daf13e9719eca0b20
coolheartvicky <162494353+coolheartvicky@users.noreply.github.com>
2024-05-19 17:52:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 IMAGE_NAME="capstoneproject"
 DOCKERFILE_PATH="Dockerfile"
-sudo docker build -t "$IMAGE_NAME" -f "$DOCKERFILE_PATH" .
+docker build -t "$IMAGE_NAME" -f "$DOCKERFILE_PATH" .
 if [ $? -eq 0 ]; then
     echo "Docker image '$IMAGE_NAME' built successfully."
 else

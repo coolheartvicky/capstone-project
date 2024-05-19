e9b43f95d54899fea408f8489c7b16bfda510a0a
coolheartvicky <162494353+coolheartvicky@users.noreply.github.com>
2024-05-19 18:22:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# Docker credentials
 DOCKER_IMAGE="capstoneproject"
 DOCKER_TAG="latest"
 DOCKER_USERNAME="vignesh110396"
+DOCKER_PASSWORD="Mar@110397"
 DOCKER_REPO="${DOCKER_USERNAME}/${DOCKER_IMAGE}:${DOCKER_TAG}"
 
-# Login to Docker Hub
+# Login to Docker Hub using environment variables
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Check if the login was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Docker login failed."
+    exit 1
+fi
 
 # Tag the Docker image
 docker tag "$DOCKER_IMAGE" "$DOCKER_REPO"

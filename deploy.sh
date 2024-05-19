a367b7e6721064be92bffe92bad27414ee5998e4
coolheartvicky <162494353+coolheartvicky@users.noreply.github.com>
2024-05-19 18:19:01 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-DOCKER_IMAGE="my-html-app"
+DOCKER_IMAGE="capstoneproject"
 DOCKER_TAG="latest"
 DOCKER_USERNAME="vignesh110396"
-DOCKER_PASSWORD="Mar@110397"
-DOCKER_REPO="$"vignesh110396"/$"my-html-app":$latest"
-echo "$Mar@110397" | docker login -u "$vignesh110396" --password-stdin
+DOCKER_REPO="${DOCKER_USERNAME}/${DOCKER_IMAGE}:${DOCKER_TAG}"
+
+# Login to Docker Hub
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Tag the Docker image
 docker tag "$DOCKER_IMAGE" "$DOCKER_REPO"
+
+# Push the Docker image
 docker push "$DOCKER_REPO"
+
+# Check if the push was successful
 if [ $? -eq 0 ]; then
     echo "Docker image '$DOCKER_REPO' pushed successfully to Docker Hub."
 else
     echo "Error: Failed to push Docker image to Docker Hub."
 fi
-
-
-# this is the deploy bash
